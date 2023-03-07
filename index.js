e61e75eb2cbe05d7f78672b552d7f45f54268083
HenaryKhuraijam <henarykhu123@gmial.com>
2023-03-07 20:34:42 +0530

    Add popup menu

---
+++ index.js
@@ -0,0 +1,7 @@
+function menuClickedHandler(e) {
+  const nav = document.getElementById('nav');
+  nav.classList.toggle("popup");
+}
+
+document.getElementById("menu").addEventListener('click', menuClickedHandler);
+document.getElementById("nav").addEventListener('click', menuClickedHandler);
