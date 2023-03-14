d2cb7d44eaedd6f38c5135bea05ab734783df640
HenaryKhuraijam <henarykhu123@gmial.com>
2023-03-14 14:46:06 +0530

    popup menu

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 function menuClickedHandler(e) {
   const nav = document.getElementById('nav');
   nav.classList.toggle("popup");
+
+ if(nav.classList.contains('popup')) {    
+  nav.addEventListener('click', menuClickedHandler);
+  }
+  else {
+    nav.removeEventListener('click', menuClickedHandler);
+  }
 }
 
-document.getElementById("menu").addEventListener('click', 'menuClickedHandler');
-document.getElementById("nav").addEventListener('click', 'menuClickedHandler');
+document.getElementById("menu").addEventListener('click', menuClickedHandler);
+
+
+function btn(){
+  const cardone = document.getElementById('cardwork1');
+  cardone.classList.toggle("popup-windows");
+  
+}
+
+document.getElementById("btn1").addEventListener('click',btn);
+
